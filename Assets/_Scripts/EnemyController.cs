9e7aed8190e23b67482682d40a9d6b201092fdaf
EmmanuelN-L <46698046+EmmanuelN-L@users.noreply.github.com>
2020-10-16 22:26:14 -0400

    Lab Done

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float horizontalSpeed;
+    public float horizontalBoundary;
+    public float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+    private void _Move()
+    {
+        transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    private void _CheckBounds()
+    {
+        //CheckBounds Right
+        if (transform.position.x >= horizontalBoundary)
+        {
+            direction = -1.0f;
+        }
+        //CheckBounds Left
+        if (transform.position.x <= -horizontalBoundary)
+        {
+            direction = 1.0f;
+        }
+    }
+}
